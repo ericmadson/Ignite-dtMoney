a0826cb84a25593bf989f9c11022aaa7dcfd369a
EricM <ericmadson67@hotmail.com>
2022-01-17 19:25:48 -0300

    (UPDATE) MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import { Container } from './styles'
 
 export function TransactionsTable () {
+    useEffect(() => {
+        fetch('http://localhost:3000/api/transactions')
+        .then(response => response.json)
+        .then(data => console.log(data))
+    }, []);
+
+
     return (
         <Container>
             <table>
@@ -17,7 +25,7 @@ export function TransactionsTable () {
                     <tr>
                         <td>Desenvolvimento de website</td>
                         <td className="deposit">R$ 12.000</td>
-                        <td>Desesnvolvimento</td>
+                        <td>Desenvolvimento</td>
                         <td>20/02/2021</td>
                     </tr>
                     <tr>
