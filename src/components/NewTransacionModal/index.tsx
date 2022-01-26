aef238412a19a2b778e9b0c24f8d3e7e62647473
EricM <ericmadson67@hotmail.com>
2022-01-26 14:29:08 -0300

    (UPDATE) Finalizando Aplicação

--- src/components/NewTransacionModal/index.tsx
+++ src/components/NewTransacionModal/index.tsx
@@ -1,10 +1,11 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState, useContext } from 'react'
+import { api } from '../../services/api'
+import { TransactionsContext } from '../../TransactionsContext'
 import Modal from 'react-modal'
+
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/entradas.svg'
 import outcomeImg from '../../assets/saídas.svg'
-import { api } from '../../services/api'
-
 
 import { Container, RadioBox, TransactionTypeContainer } from './styles'
 
@@ -14,22 +15,29 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const {createTransaction} = useContext(TransactionsContext)
+
     const[title, setTitle] = useState('')
-    const[value, setValue] = useState(0)
+    const[amount, setAmount] = useState(0)
     const[category, setCategory] = useState('')
     const[type, setType] = useState('deposit')
     
-    function handleCreateNewTransaction (event: FormEvent) {
+    async function handleCreateNewTransaction (event: FormEvent) {
         event.preventDefault();
 
-        const data = {
-            title, 
-            value, 
-            type, 
+        await createTransaction ({
+            title,
+            amount,
             category,
-        };
+            type,
+        });
 
-        api.post('/transactions', data)
+        setTitle('')
+        setAmount(0)
+        setCategory('')
+        setType('deposit')
+
+        onRequestClose ();
     }
 
     return (
@@ -54,8 +62,8 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
                 <input 
                 type="number" 
                 placeholder="Valor" 
-                value={value}
-                onChange={event => setValue(Number(event.target.value))}/>
+                value={amount}
+                onChange={event => setAmount(Number(event.target.value))}/>
 
                 < TransactionTypeContainer>
                     <RadioBox 
@@ -85,4 +93,8 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
             </Container>
         </Modal>
     )
+}
+
+function TransacrionsContext(TransacrionsContext: any) {
+    throw new Error('Function not implemented.')
 }
